b04c519d46b9e42c8010e9463c7faba59cc0a2f3
codeworkx <codeworkx@cyanogenmod.org>
2016-12-26 20:54:07 +0100

    Snap: enable redeye reduction setting
    Change-Id: I992a71a5d5b090b73e7294dbb0c6511aae54d885

--- src/com/android/camera/CaptureUI.java
+++ src/com/android/camera/CaptureUI.java
@@ -113,6 +113,7 @@ public class CaptureUI extends BaseUI implements PreviewGestures.SingleTapListen
             SettingsManager.KEY_ISO,
             SettingsManager.KEY_WHITE_BALANCE,
             SettingsManager.KEY_FACE_DETECTION,
+            SettingsManager.KEY_REDEYE_REDUCTION,
             SettingsManager.KEY_VIDEO_HIGH_FRAME_RATE,
             SettingsManager.KEY_VIDEO_FLASH_MODE,
             SettingsManager.KEY_VIDEO_DURATION,
@@ -124,7 +125,6 @@ public class CaptureUI extends BaseUI implements PreviewGestures.SingleTapListen
             SettingsManager.KEY_CAMERA2
     };
     String[] mDeveloperKeys = new String[]{
-            SettingsManager.KEY_REDEYE_REDUCTION,
             SettingsManager.KEY_MONO_ONLY,
             SettingsManager.KEY_CLEARSIGHT,
             SettingsManager.KEY_MONO_PREVIEW,
